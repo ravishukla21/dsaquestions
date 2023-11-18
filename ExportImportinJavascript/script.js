fc267d7155e5f22c8dbeaf623fc4dc66151ef3fb
ravishukla21 <ravi21.434@gmail.com>
2023-11-18 23:02:13 +0530

    hi

---
+++ ExportImportinJavascript/script.js
@@ -0,0 +1,18 @@
+// import { hello  } from "./utility.js";
+// let abc=hello();
+// console.log(abc);
+
+
+//import { hello as helloworld,mul } from "./utility.js";
+// import * as utility from "./utility.js";
+
+// let abc=utility.hello();
+// console.log(abc);
+// let mul1=utility.mul(2,3,4)
+// console.log(mul1)
+
+// console.log(utility.Students)
+
+import multiply from "./utility.js";
+const ab=multiply(2,4,6)
+console.log(ab,"ab")
---
+++ ExportImportinJavascript/utility.js
@@ -0,0 +1,15 @@
+ const helloworld=()=>{
+    return "hellow world in console"
+}
+
+export default function multiply(a,b,c){
+    return a*b*c
+}
+
+const addition=(a,b,c)=>{
+    return a+b+c
+}
+// export {hello,multiply,addition}
+
+export const Students=23;
+export {multiply as mul,helloworld as hello}
